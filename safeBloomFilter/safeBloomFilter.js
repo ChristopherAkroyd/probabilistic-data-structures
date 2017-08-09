708660bc806072fc642ef3f3d09b4dbcc33a68aa
Chris Akroyd <cra5@kent.ac.uk>
2017-08-09 23:14:29 +0100

    - Minor refactoring of safeBloomFilter (Name change to estimateFilterSize). - Fixed bug where optimal hash functions was calculated incorrectly, (Needs a review before I commit to it.) - Added .addSafe(key) which adds a check to prevent the addition of an item to the filter if the filter would go above it's capacity. - Added comments.

--- safeBloomFilter/safeBloomFilter.js
+++ safeBloomFilter/safeBloomFilter.js
@@ -2,21 +2,63 @@ const BloomFilter = require('../bloomFilter/bloomfilter.js');
 
 class SafeBloomFilter extends BloomFilter {
   constructor(expectedInserts, falsePositiveRate) {
+    // @TODO review this with the aim to remove the double static func call.
     super(
-      SafeBloomFilter.calculateSize(expectedInserts, falsePositiveRate),
-      SafeBloomFilter.optimalNumHashFunctions(expectedInserts, falsePositiveRate),
+      SafeBloomFilter.estimateNumberBits(expectedInserts, falsePositiveRate),
+      SafeBloomFilter.optimalNumHashFunctions(expectedInserts,
+        SafeBloomFilter.estimateNumberBits(expectedInserts, falsePositiveRate)),
     );
     this.capacity = expectedInserts;
   }
 
-  static calculateSize(expectedInserts, falsePositiveRate) {
+  /**
+   * Estimates the number of bits required to store the given number of elements
+   * while maintaining the given false positive rate.
+   *
+   * m = - (n Ln P / (Ln 2)^2)
+   *
+   * https://en.wikipedia.org/wiki/Bloom_filter
+   *
+   * @param {number} expectedInserts
+   * @param {number} falsePositiveRate
+   * @returns {number} - Number of bits this filter requires.
+   */
+  static estimateNumberBits(expectedInserts, falsePositiveRate) {
     const ln2Sq = Math.LN2 ** 2;
     const nLnP = expectedInserts * Math.log(falsePositiveRate);
-    return Math.ceil(nLnP / -ln2Sq);
+    return Math.ceil(-1 * (nLnP / ln2Sq));
   }
 
+  /**
+   * Calculates the optimal number of hash functions to minimise the false probability
+   * for the given m (size) and n (expectedInserts).
+   *
+   * k = (m / n) * ln(2).
+   *
+   * https://en.wikipedia.org/wiki/Bloom_filter
+   *
+   * @param {number} expectedInserts
+   * @param {number} size
+   * @returns {number} - Number of Hash functions this filter requires.
+   */
   static optimalNumHashFunctions(expectedInserts, size) {
     return (expectedInserts / size) * Math.LN2;
+  }
+
+  /**
+   * Only adds an item to the filter if we are below the capacity of the filter,
+   * this avoids increasing the actual error rate of the filter above the desired
+   * error rate.
+   *
+   * @param key
+   * @return {boolean} whether the key was successfully added or not.
+   */
+  addSafe(key) {
+    if (this.count < this.capacity) {
+      this.add(key);
+      return true;
+    }
+    return false;
   }
 }
 
